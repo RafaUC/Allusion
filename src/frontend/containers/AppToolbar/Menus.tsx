a05bf3b6d8fedb2796a2df468acf2c138fdb1316
RafaUC <rafa_ruc6@hotmail.com>
2025-11-27 01:14:07 -0600

    - Feature: Add a thumbnail corner radius slider to the Change View content panel.

--- src/frontend/containers/AppToolbar/Menus.tsx
+++ src/frontend/containers/AppToolbar/Menus.tsx
@@ -46,6 +46,9 @@ export const ViewCommand = () => {
       <MenuDivider />
 
       <ThumbnailSizeSliderMenuItem />
+      <br />
+      <ThumbnailRadiusSliderMenuItem />
+      <br />
       <ThumbnailPaddingSizeSliderMenuItem />
     </MenuButton>
   );
@@ -153,6 +156,60 @@ const thumbnailSizeOptions = [
   { value: 608 },
 ];
 
+const thumbnailRadiusOptions = [
+  { value: 0, label: '0%' },
+  { value: 1 },
+  { value: 2 },
+  { value: 3 },
+  { value: 4 },
+  { value: 5 },
+  { value: 6 },
+  { value: 7 },
+  { value: 8 },
+  { value: 9 },
+  { value: 10, label: '10%' },
+  { value: 11 },
+  { value: 12 },
+  { value: 13 },
+  { value: 14 },
+  { value: 15 },
+  { value: 16 },
+  { value: 17 },
+  { value: 18 },
+  { value: 19 },
+  { value: 20, label: '20%' },
+  { value: 21 },
+  { value: 22 },
+  { value: 23 },
+  { value: 24 },
+  { value: 25 },
+  { value: 26 },
+  { value: 27 },
+  { value: 28 },
+  { value: 29 },
+  { value: 30, label: '30%' },
+  { value: 31 },
+  { value: 32 },
+  { value: 33 },
+  { value: 34 },
+  { value: 35 },
+  { value: 36 },
+  { value: 37 },
+  { value: 38 },
+  { value: 39 },
+  { value: 40, label: '40%' },
+  { value: 41 },
+  { value: 42 },
+  { value: 43 },
+  { value: 44 },
+  { value: 45 },
+  { value: 46 },
+  { value: 47 },
+  { value: 48 },
+  { value: 49 },
+  { value: 50, label: '50%' },
+];
+
 const thumbnailPaddingSizeOptions = [
   { value: 0, label: '0' },
   { value: 1 },
@@ -175,6 +232,16 @@ const thumbnailPaddingSizeOptions = [
   { value: 18 },
   { value: 19 },
   { value: 20, label: '20' },
+  { value: 21 },
+  { value: 22 },
+  { value: 23 },
+  { value: 24 },
+  { value: 25 },
+  { value: 26 },
+  { value: 27 },
+  { value: 28 },
+  { value: 29 },
+  { value: 30, label: '30' },
 ];
 
 export const LayoutMenuItems = observer(() => {
@@ -226,6 +293,22 @@ export const ThumbnailSizeSliderMenuItem = observer(() => {
       min={thumbnailSizeOptions[0].value}
       max={thumbnailSizeOptions[thumbnailSizeOptions.length - 1].value}
       step={20}
+    />
+  );
+});
+
+export const ThumbnailRadiusSliderMenuItem = observer(() => {
+  const { uiStore } = useStore();
+  return (
+    <MenuSliderItem
+      value={uiStore.thumbnailRadius}
+      label="Thumbnail Corner Radius"
+      onChange={uiStore.setThumbnailRadius}
+      id="thumbnail-padding-sizes"
+      options={thumbnailRadiusOptions}
+      min={thumbnailRadiusOptions[0].value}
+      max={thumbnailRadiusOptions[thumbnailRadiusOptions.length - 1].value}
+      step={1}
     />
   );
 });
